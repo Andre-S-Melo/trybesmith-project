1719579498fd78b8f1c145e7d6f5d136063fe795
André Melo <adr.smelo@gmail.com>
2022-07-29 18:29:37 -0300

    feat: (product.controller) implementa método getAll junto a camada Controllers para o endpoint GET /products

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -8,6 +8,12 @@ class ProductController {
     this.service = service;
   }
 
+  public getAll = async (req: Request, res: Response) => {
+    const products = await this.service.getAll();
+
+    return res.status(200).json(products);
+  };
+
   public create = async (req: Request, res: Response) => {
     const product = req.body;
     const productCreated = await this.service.create(product);
