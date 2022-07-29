79af5918144dbea95f143632d13c86266c3bf92c
André Melo <adr.smelo@gmail.com>
2022-07-29 17:25:14 -0300

    feat: (product.routes) implementa arquivo de rotas para products

---
+++ src/routes/product.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import ProductController from '../controllers/product.controller';
+
+const productRouter = Router();
+
+const productController = new ProductController();
+
+productRouter.post('/products', productController.create);
+
+export default productRouter;
