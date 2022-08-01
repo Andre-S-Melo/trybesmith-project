e7bec4c42a9f0c4a1a93954026f0284a4ae125b7
André Melo <adr.smelo@gmail.com>
2022-08-01 16:27:54 -0300

    feat: (order.routes) implementa arquivo de rotas para orders

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRouter from './routes/user.routes';
+import orderRouter from './routes/order.routes';
 import productRouter from './routes/product.routes';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(userRouter);
+app.use(orderRouter);
 app.use(productRouter);
 
 export default app;
