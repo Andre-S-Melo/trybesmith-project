0a9c8beaf031a023ff585d1700d3f154706a4425
André Melo <adr.smelo@gmail.com>
2022-08-01 11:59:15 -0300

    feat: (user.routes) implementa arquivo de rotas para users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import userRouter from './routes/user.routes';
 import productRouter from './routes/product.routes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(userRouter);
 app.use(productRouter);
 
 export default app;
