6a8c560bbb922b5e428de943570e257c337cfb05
André Melo <adr.smelo@gmail.com>
2022-08-01 11:55:14 -0300

    feat: (user.service) implementa método create junto a camada Services para o endpoint POST /users

---
+++ src/services/user.service.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import jwt, { SignOptions } from 'jsonwebtoken';
+import connection from '../models/connection';
+import UserModel from '../models/user.model';
+import User from '../interfaces/user.interface';
+
+dotenv.config();
+
+class UserService {
+  public model: UserModel;
+
+  constructor(model: UserModel = new UserModel(connection)) {
+    this.model = model;
+  }
+
+  public async create(user: User) {
+    const newUser = await this.model.create(user);
+
+    const jwtConfig: SignOptions = {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign({ data: newUser.username }, process.env.JWT_SECRET!, jwtConfig);
+
+    return { token };
+  }
+}
+
+export default UserService;
